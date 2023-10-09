2254e66678c76fe16a3d70f38d4a5a484c8c0a39
mionaga <143914849+mionaga@users.noreply.github.com>
2023-10-09 12:59:39 +0000

    favorite-js

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.page(params[:page])
     @book = Book.new
   end
 
